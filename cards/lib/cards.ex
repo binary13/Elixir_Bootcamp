4512534a4cbb7705c7ca16ce9042acf9c4b71ac7
Justin Neil <justinmneil@gmail.com>
2016-11-21 15:27:20 -0500

    Initial Commit

---
+++ cards/lib/cards.ex
@@ -0,0 +1,15 @@
+defmodule Cards do
+    def create_deck do
+        values = ["Ace", "Two", "Three", "Four", "Five"]
+        suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+        deck = Enum.zip(values, suits)
+    end
+
+    def shuffle(deck) do
+        Enum.shuffle(deck)
+    end
+
+    def contains?(deck, card) do
+        Enum.member?(deck, card)
+    end
+end
